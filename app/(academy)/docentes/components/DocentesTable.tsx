f6ff577492520f48a2a19e0d4238518327e2dccc
Josué Espinoza <142639857+josueespinoza2004@users.noreply.github.com>
2026-05-05 15:13:09 -0600

    Increase max width of DocentesTable and EstudiantesTable components for improved layout

--- app/(academy)/docentes/components/DocentesTable.tsx
+++ app/(academy)/docentes/components/DocentesTable.tsx
@@ -186,7 +186,7 @@ export default function DocentesTable({ docentes: initial }: Props) {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-4">
+    <div className="max-w-7xl mx-auto p-4">
       {viewDocente && (
         <DocenteDetalle
           docente={viewDocente}
--- app/(academy)/estudiantes/components/EstudiantesTable.tsx
+++ app/(academy)/estudiantes/components/EstudiantesTable.tsx
@@ -174,7 +174,7 @@ export default function EstudiantesTable({ estudiantes: initial }: Props) {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-4">
+    <div className="max-w-7xl mx-auto p-4">
       {viewStudent && (
         <EstudianteDetalle
           estudiante={viewStudent}
